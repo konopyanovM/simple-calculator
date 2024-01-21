3c8aab3ff1dbcdbac0ffbd06f6218001d42b1e58
Manap <konopyanov.manap@gmail.com>
2024-01-21 17:58:33 +0600

    feat: added calculator styling

---
+++ src/app/core/types/enums.ts
@@ -0,0 +1,6 @@
+export enum SignEnum {
+  Addition = '+',
+  Subtraction = '−',
+  Multiplication = '×',
+  Division = '/',
+}
--- src/app/features/calculator/calculator.component.ts
+++ src/app/features/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SignEnum} from "../../core/types";
 
 @Component({
   selector: 'app-calculator',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class CalculatorComponent {
 
+  protected readonly SignEnum = SignEnum;
 }
